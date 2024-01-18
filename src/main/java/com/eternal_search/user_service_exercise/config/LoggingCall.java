a061c97aee32980377ae6c13d7399f24384257ab
Ivan KOLESNIKOV <kiv.apple@gmail.com>
2024-01-18 17:57:10 +0100

    Added a comment

--- src/main/java/com/eternal_search/user_service_exercise/config/LoggingCall.java
+++ src/main/java/com/eternal_search/user_service_exercise/config/LoggingCall.java
@@ -1,4 +1,8 @@
 package com.eternal_search.user_service_exercise.config;
 
+/**
+ * Une annotation utilisée pour permettre la journalisation
+ * des entrées, des sorties et du temps écoulé de la méthode.
+ */
 public @interface LoggingCall {
 }
